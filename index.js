6bdc56df450383c6e0cedfb86b2522433892f45f
aki_ooki <ookiakieml@gmail.com>
2025-09-27 03:49:52 +0900

    modan javascript 9：2025/09/27
    DefaultをNamedにしてRe-Export

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import pika from "./pokemon/index.js";
+import { pika } from "./pokemon/index.js";
 
 document.body.textContent = pika;
--- pokemon/index.js
+++ pokemon/index.js
@@ -1,1 +1,1 @@
-export { default } from "./pokemon.js";
+export { default as pika } from "./pokemon.js";
